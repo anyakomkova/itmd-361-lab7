4eea4d51a6288562bf4bdbef1296894f6dc05070
anyakomkova <122331580+anyakomkova@users.noreply.github.com>
2023-04-21 18:05:36 -0500

    added js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 function init(){
-    //add your javascrip between these two lines of code
-     
+  var button = document.getElementById('entrybutton');
+  
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Anna Komkova: " + textbox.value);
+  }
+  
+  button.addEventListener('click', showMeText);
+  }
     
-    
-    
-    
-    
-    
-    
-    window.addEventListener('load', init);
-    
+  window.addEventListener('load', init);
